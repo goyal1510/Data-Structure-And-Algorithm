a0e7938135f4274536a83dd7f5e66e2eabbc4d52
Jayant <98017109+goyal1510@users.noreply.github.com>
2023-06-27 18:01:09 +0530

    Day1_completed (#1)

---
+++ Day1/datatypes.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    char c='r';
+    float f = 22.4;
+    bool flag = true;
+    cout<<sizeof(a)<<endl;
+    cout<<sizeof(c)<<endl;
+    cout<<sizeof(f)<<endl;
+    cout<<sizeof(flag)<<endl;
+}
