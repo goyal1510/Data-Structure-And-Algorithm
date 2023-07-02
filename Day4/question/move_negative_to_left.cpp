07a1ad4b7cfdda43066733dffd7582978e2be9b2
Jayant <goyal151002@gmail.com>
2023-07-02 20:29:28 +0530

    array

---
+++ Day4/question/move_negative_to_left.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> arr{1,2,3,-4,-5,3,-2};
+    int i = 0 ; 
+    int h = arr.size()-1;
+    while(i<h){
+        if(arr[i]<0){
+            i++;
+        }
+        else if(arr[h]>0){
+            h--;
+        }
+        else swap(arr[i],arr[h]);
+    }
+    for(int i = 0 ; i < arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
---
+++ Day4/question/tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+while(i<h){
+    //     if(arr[i]<0){
+    //         i++;
+    //     }
+    //     else if(arr[h]>0){
+    //         h++;
+    //     }
+    //     else swap(arr[i],arr[h]);
+    // }
