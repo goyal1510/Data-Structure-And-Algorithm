313874b0cae639f848a0956b2339233e364d3b2f
goyal1510 <goyal151002@gmail.com>
2024-02-28 16:08:49 +0530

    Question updated

--- Day1/Patterns/half_pyramid.cpp
+++ Day1/Patterns/half_pyramid.cpp
@@ -8,12 +8,11 @@
 using namespace std;
 int main()
 {
-
     int num;
     cin >> num;
     for (int i = 0; i < num; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = 0; j <= i; j++)
         {
             cout << "* ";
         }
